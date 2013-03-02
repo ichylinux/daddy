43fcf1f6840d350c1da7bd182daa4311599d2981
ichy <ichy@localhost>
2013-03-02 14:55:39 +0900

    database generator

--- lib/daddy/railtie.rb
+++ lib/daddy/railtie.rb
@@ -5,7 +5,10 @@ require 'rails'
 module Daddy
   class Railtie < Rails::Railtie
     rake_tasks do
-      require 'tasks/create_databases.rake'
+      lib = File.dirname(File.dirname(__FILE__))
+      Dir[File.join(lib, 'tasks/*.rake')].each do |f|
+        load f
+      end
     end
   end
 end
