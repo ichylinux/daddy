d3e60ea27c691686e82658904312136f396ac9c5
ichy <ichy@localhost>
2013-03-10 04:15:21 +0900

    p2

--- sample_4/features/step_definitions/開発日記/2013-03-08.rb
+++
@@ -1,4 +0,0 @@
-# coding: UTF-8
-
-前提(/^列「introduction」を追加$/) do
-end
--- sample_5/db/migrate/20130307134703_rename_column_name_to_last_name_on_careers.rb
+++
@@ -1,9 +0,0 @@
-class RenameColumnNameToLastNameOnCareers < ActiveRecord::Migration
-  def up
-    rename_column :careers, :name, :last_name
-  end
-
-  def down
-    rename_column :careers, :last_name, :name
-  end
-end
