ac90e3ffa3202d317d638c37806e43f301a52c4c
ichy <ichylinux@gmail.com>
2013-03-11 01:50:13 +0900

    p1

---
+++ careerlife/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Careerlife::Application.config.secret_token = '437732a2c065847a12b8ff24229d429f2885b9355cc9fc86a7bb36d80c5a6153535444d9922ba891bb926400cc58fdec3475aa61076f9f57b0c34f136a5e0611'
