083521302fbf6e69adcc7f54b56025ba6c2ed399
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-03-14 12:39:46 +0900

    p2

---
+++ careerlife/features/step_definitions/仕様書/steps.rb
@@ -0,0 +1,5 @@
+# coding: UTF-8
+
+前提(/^トップページを表示$/) do
+  assert_visit '/'
+end
---
+++ careerlife/features/step_definitions/開発日記/2013-03-06.rb
@@ -0,0 +1,15 @@
+# coding: UTF-8
+
+もし /^rails g controller top$/ do
+end
+
+もし(/^アクション「index」を実装$/) do
+  puts "<pre>#{`git diff origin/p1 -- app`}</pre>"
+end
+
+もし(/^root の指定を変更$/) do
+  puts "<pre>#{`git diff origin/p1 -- config/routes.rb`}</pre>"
+end
+
+もし(/^rm public\/index\.html$/) do
+end
