12f5822c503cd5dfb8942b3225225ae377294f2d
ichy <ichylinux@gmail.com>
2013-03-16 01:39:03 +0900

    整理

--- careerlife/test/functional/careers_controller_test.rb
+++ careerlife/test/functional/careers_controller_test.rb
@@ -1,49 +1,4 @@
 require 'test_helper'
 
 class CareersControllerTest < ActionController::TestCase
-  setup do
-    @career = careers(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:careers)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create career" do
-    assert_difference('Career.count') do
-      post :create, career: { birthday: @career.birthday, gender: @career.gender, name: @career.name }
-    end
-
-    assert_redirected_to career_path(assigns(:career))
-  end
-
-  test "should show career" do
-    get :show, id: @career
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @career
-    assert_response :success
-  end
-
-  test "should update career" do
-    put :update, id: @career, career: { birthday: @career.birthday, gender: @career.gender, name: @career.name }
-    assert_redirected_to career_path(assigns(:career))
-  end
-
-  test "should destroy career" do
-    assert_difference('Career.count', -1) do
-      delete :destroy, id: @career
-    end
-
-    assert_redirected_to careers_path
-  end
 end
