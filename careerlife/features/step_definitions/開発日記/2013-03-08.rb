50d80b5678773f7ffa5c5b4b2fd562dc7d33dde6
ichy <ichylinux@gmail.com>
2013-03-19 22:55:03 +0900

    p4

--- careerlife/features/step_definitions/開発日記/2013-03-08.rb
+++ careerlife/features/step_definitions/開発日記/2013-03-08.rb
@@ -1,4 +1,15 @@
 # coding: UTF-8
 
-前提(/^列「introduction」を追加$/) do
+前提 /^日本語ロケールを用意$/ do
+  file = 'config/locales/ja.yml'
+  puts file
+  puts "<pre>#{File.read(file)}</pre>"
+
+  file = 'config/locales/messages.ja.yml'
+  puts file
+  puts "<pre>#{File.read(file)}</pre>"
+end
+
+前提 /^erb を修正$/ do
+  git_diff 'app/views/careers/index.html.erb', 'careerlife/app/views/careers/index.html.erb'
 end
