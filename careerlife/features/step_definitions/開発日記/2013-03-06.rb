c4d8e98059ba411383394eec01dfefc1f6181b2b
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-03-19 13:12:54 +0900

    p3

--- careerlife/features/step_definitions/開発日記/2013-03-06.rb
+++ careerlife/features/step_definitions/開発日記/2013-03-06.rb
@@ -1,7 +1,9 @@
 # coding: UTF-8
 
-前提(/^rails g scaffold career name:string birthday:date gender:string$/) do |string|
+前提(/^rails g scaffold career name:string birthday:date gender:string$/) do
+  puts "<pre>#{`git diff origin/p2 --name-only app db test`}</pre>"
 end
 
 前提(/^rake db:migrate$/) do
+  puts "<pre>#{File.read('db/schema.rb')}</pre>"
 end
--- careerlife/features/開発日記/2013-03-06.feature
+++ careerlife/features/開発日記/2013-03-06.feature
@@ -5,7 +5,4 @@
 シナリオ: キャリアの登録画面を作成
 
 * rails g scaffold career name:string birthday:date gender:string
-"""
-　rails の scaffold の機能を利用することでCRUD画面を自動生成できます。
-"""
 * rake db:migrate
