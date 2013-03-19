e8e42a25527c0fc24dbe11e69f6ab0cda5935f7b
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-03-19 12:49:16 +0900

    p2

--- careerlife/features/step_definitions/開発日記/2013-03-06.rb
+++ careerlife/features/step_definitions/開発日記/2013-03-06.rb
@@ -1,14 +1,16 @@
 # coding: UTF-8
 
 もし /^rails g controller top$/ do
+  puts "<pre>#{`git diff origin/p1 --name-only app test`}</pre>"
 end
 
 もし(/^アクション「index」を実装$/) do
-  puts "<pre>#{`git diff origin/p1 -- app`}</pre>"
+  git_diff 'app/controllers/top_controller.rb', 'careerlife/app/controllers/top_controller.rb'
+  git_diff 'app/views/top/index.html.erb', 'careerlife/app/views/top/index.html.erb'
 end
 
 もし(/^root の指定を変更$/) do
-  puts "<pre>#{`git diff origin/p1 -- config/routes.rb`}</pre>"
+  git_diff 'config/routes.rb', 'careerlife/config/routes.rb'
 end
 
 もし(/^rm public\/index\.html$/) do
