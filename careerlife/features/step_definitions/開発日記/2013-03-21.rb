08973e1e685e2416f65ea1bf86c8cbe65a866684
ichy <ichylinux@gmail.com>
2013-03-21 23:29:26 +0900

    master

--- careerlife/features/step_definitions/開発日記/2013-03-21.rb
+++
@@ -1,5 +0,0 @@
-# coding: UTF-8
-
-前提 /^careers#create のテストを追加$/ do
-  show 'features/仕様書/02.キャリア/02.バリデーション.feature'
-end
--- careerlife/features/開発日記/2013-03-21.feature
+++
@@ -1,6 +0,0 @@
-# language: ja
-
-機能:
-
-シナリオ: カバレッジを向上させる
-  * careers#create のテストを追加
