4e4f76c3aab23cb696577923c06ec610730f55d9
ichy <ichylinux@gmail.com>
2013-03-24 18:33:57 +0900

    master

--- lib/daddy/cucumber/diff.rb
+++ lib/daddy/cucumber/diff.rb
@@ -10,8 +10,21 @@ module Daddy
         b = git.show_previous(git_path, true).gsub(/[<>]/, '<' => '&lt;', '>' => '&gt;')
         diff = Differ.diff(a, b)
 
+        diff_lines = diff.to_s.split("\n")
+        lines = []
+        puts diff_lines.size
+        diff_lines.each_with_index do |line, i|
+          if line.index('<ins ') and line.index('<del ')
+            split = line.split('<ins ')
+            lines << split[0]
+            lines << '<ins ' + split[1]
+          else
+            lines << line
+          end
+        end
+
         puts local_file
-        puts "<pre>#{diff}</pre>"
+        puts "<pre>#{lines.join("\n")}</pre>"
       end
 
       def show(file)
