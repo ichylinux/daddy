8ce19bd31c7f9a3faa2e2ab53bab6e48eea768f9
ichy <ichylinux@gmail.com>
2013-03-24 10:51:39 +0900

    deploy

--- careerlife/config/deploy.rb
+++ careerlife/config/deploy.rb
@@ -36,7 +36,7 @@ set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do 
-    run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
+    run "cd #{current_path} && #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
   end
   task :stop, :roles => :app, :except => { :no_release => true } do 
     run "#{try_sudo} kill `cat #{unicorn_pid}`"
