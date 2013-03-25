7be46ad9505c4f4fc56b9a43b04f96af5f517a9e
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-03-25 12:33:36 +0900

    p2

--- careerlife/features/step_definitions/開発日記/2013-03-06.rb
+++ careerlife/features/step_definitions/開発日記/2013-03-06.rb
@@ -4,12 +4,12 @@
   puts "<pre>#{`git diff origin/p1 --name-only app test`}</pre>"
 end
 
-もし(/^アクション「index」を実装$/) do
+もし /^アクション「index」を実装$/ do
   git_diff 'app/controllers/top_controller.rb', 'careerlife/app/controllers/top_controller.rb'
   git_diff 'app/views/top/index.html.erb', 'careerlife/app/views/top/index.html.erb'
 end
 
-もし(/^root の指定を変更$/) do
+もし /^root の指定を変更$/ do
   git_diff 'config/routes.rb', 'careerlife/config/routes.rb'
 end
 
--- lib/daddy/cucumber/diff.rb
+++ lib/daddy/cucumber/diff.rb
@@ -8,10 +8,48 @@ module Daddy
         git = Daddy::Git.new
         a = File.read(local_file).gsub(/[<>]/, '<' => '&lt;', '>' => '&gt;')
         b = git.show_previous(git_path, true).gsub(/[<>]/, '<' => '&lt;', '>' => '&gt;')
-        diff = Differ.diff(a, b)
+        diff = format_diff(Differ.diff(a, b))
 
         puts local_file
         puts "<pre>#{diff}</pre>"
+      end
+      
+      def diff(file_a, file_b)
+        a = File.read(file_a)
+        b = File.read(file_b)
+        diff = format_diff(Differ.diff(a, b))
+
+        puts file_a
+        puts "<pre>#{diff}</pre>"
+      end
+
+      def show(file)
+        puts file
+        puts "<pre>#{File.read(file).gsub(/[<>]/, '<' => '&lt;', '>' => '&gt;')}</pre>"
+      end
+      
+      private
+      
+      def format_diff(diff)
+        lines = []
+
+        diff_lines = diff.to_s.split("\n")
+        diff_lines.each_with_index do |line, i|
+          if line.index('</del><ins class="differ">') 
+            split = line.split('</del><ins class="differ">')
+            lines << split[0]
+            if split[1].start_with?('"')
+              lines << '</del><ins class="differ">"'
+              lines << split[1][1..-1]
+            else
+              lines << '</del><ins class="differ">' + split[1]
+            end
+          else
+            lines << line
+          end
+        end
+        
+        lines.join("\n")
       end
 
     end
