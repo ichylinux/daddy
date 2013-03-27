b3cf1eb63c2f17a41ef5cee1c67b2e321985128a
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-03-27 12:39:48 +0900

    prefix

--- careerlife/config/deploy.rb
+++ careerlife/config/deploy.rb
@@ -13,6 +13,8 @@ set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_subdir, application
 set :strategy, Daddy::Capistrano::RemoteCacheSubdir.new(self)
 
+set :asset_env, "#{asset_env} RAILS_RELATIVE_URL_ROOT=/careerlife"
+
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
