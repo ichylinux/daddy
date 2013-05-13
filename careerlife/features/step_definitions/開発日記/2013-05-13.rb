d12280d9561776d4f1a95a89f5e33553e36f5b87
ichy <ichylinux@gmail.com>
2013-05-13 20:52:14 +0900

    ユーザとキャリアの紐付け

--- careerlife/features/step_definitions/開発日記/2013-05-13.rb
+++ careerlife/features/step_definitions/開発日記/2013-05-13.rb
@@ -1,2 +1,41 @@
 # coding: UTF-8
 
+前提 /^rails g migration add_column_last_name_on_users$/ do
+  show 'db/migrate/20130513034945_add_column_last_name_on_users.rb', :as => 'edit'
+end
+
+前提 /^rails g migration add_column_first_name_on_users$/ do
+  show 'db/migrate/20130513034954_add_column_first_name_on_users.rb', :as => 'edit'
+end
+
+前提 /^rails g migration add_column_gender_on_users$/ do
+  show 'db/migrate/20130513034959_add_column_gender_on_users.rb', :as => 'edit'
+end
+
+前提 /^rails g migration add_column_birthday_on_users$/ do
+  show 'db/migrate/20130513035004_add_column_birthday_on_users.rb', :as => 'edit'
+end
+
+前提 /^rails g migration add_column_deleted_on_careers$/ do
+  show 'db/migrate/20130513035957_add_column_deleted_on_careers.rb', :as => 'edit'
+end
+
+前提 /^rails g migration remove_column_last_name_on_users$/ do
+  show 'db/migrate/20130513040300_remove_column_last_name_on_users.rb', :as => 'edit'
+end
+
+前提 /^rails g migration remove_column_first_name_on_users$/ do
+  show 'db/migrate/20130513040307_remove_column_first_name_on_users.rb', :as => 'edit'
+end
+
+前提 /^rails g migration remove_column_gender_on_users$/ do
+  show 'db/migrate/20130513040312_remove_column_gender_on_users.rb', :as => 'edit'
+end
+
+前提 /^rails g migration remove_column_birthday_on_users$/ do
+  show 'db/migrate/20130513040317_remove_column_birthday_on_users.rb', :as => 'edit'
+end
+
+前提 /^rake db:migrate$/ do
+  git_diff 'db/schema.rb', :as => 'auto'
+end
