f9b6be1b4fdcf52e0a4c5def208e2da5b3cdac46
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-07-04 13:03:23 +0900

    p13

--- careerlife/features/step_definitions/開発日記/2013-07-04.rb
+++ careerlife/features/step_definitions/開発日記/2013-07-04.rb
@@ -3,3 +3,8 @@
 もし /^content_for で拡張の余地を設ける$/ do
   git_diff 'app/views/layouts/application.html.erb'
 end
+
+もし /^プロフィール機能とキャリア機能をサイドバー付きのレイアウトに変更$/ do
+  git_diff 'app/views/layouts/profiles.html.erb', :as => 'new'
+  git_diff 'app/controllers/careers_controller.rb'
+end
