fd733587711a92806d72af843ac8e97812930b1e
ichy <ichylinux@gmail.com>
2013-07-20 18:47:48 +0900

    csv fixtures をサポート

--- daddy.gemspec
+++ daddy.gemspec
@@ -16,6 +16,7 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'cucumber'
   s.add_runtime_dependency 'cucumber-rails'
   s.add_runtime_dependency 'ci_reporter'
+  s.add_runtime_dependency 'database_cleaner'
   s.add_runtime_dependency 'differ'
   s.add_runtime_dependency 'poltergeist'
   s.add_runtime_dependency 'rails', ['~> 3.2.0']
--- lib/daddy/cucumber.rb
+++ lib/daddy/cucumber.rb
@@ -50,11 +50,32 @@ AfterConfiguration do |configuration|
   }
 end
 
+begin
+  require 'database_cleaner'
+  require 'database_cleaner/cucumber'
+
+  DatabaseCleaner.strategy = :truncation
+rescue NameError
+  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+end
+
 Before do
+  DatabaseCleaner.start
+
   ActiveRecord::Fixtures.reset_cache
   fixtures_folder = File.join(Rails.root, 'test', 'fixtures')
   fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
   ActiveRecord::Fixtures.create_fixtures(fixtures_folder, fixtures)
+  if defined? RailsCsvFixtures
+    fixtures = Dir[File.join(fixtures_folder, '*.csv')].map {|f| File.basename(f, '.csv') }
+    ActiveRecord::Fixtures.create_fixtures(fixtures_folder, fixtures)
+  end
+end
 
+After do |scenario|
+  DatabaseCleaner.clean
+end
+
+Before do
   resize_window(1280, 720)
 end
