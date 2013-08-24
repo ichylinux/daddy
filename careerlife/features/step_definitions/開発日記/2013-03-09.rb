0352249c70744ef0537756071239fd5a713bc20b
ichy <ichylinux@gmail.com>
2013-08-24 12:48:46 +0900

    p5

--- careerlife/features/step_definitions/開発日記/2013-03-09.rb
+++ careerlife/features/step_definitions/開発日記/2013-03-09.rb
@@ -5,22 +5,22 @@
 end
 
 前提 /^ヘッダを作成$/ do
-  git_diff 'app/views/common/_header.html.erb'
+  show 'app/views/common/_header.html.erb', :as => 'new'
 end
 
 前提 /^rails g model career_detail$/ do
-  git_diff 'db/migrate/20130308174601_create_career_details.rb'
+  show 'db/migrate/20130308174601_create_career_details.rb', :as => ['auto', 'edit']
 end
 
 前提 /^rake db:migrate$/ do
-  git_diff 'db/schema.rb'
+  git_diff 'db/schema.rb', :as => 'auto', :from => 14, :to => 27
 end
 
 前提 /^view を修正$/ do
   git_diff 'app/views/careers/_form.html.erb'
-  git_diff 'app/views/careers/_career_detail_fields.html.erb'
+  show 'app/views/careers/_career_detail_fields.html.erb', :as => 'new'
 end
 
 前提 /^controller を修正$/ do
-  git_diff 'app/controllers/careers_controller.rb'
+  git_diff 'app/controllers/careers_controller.rb', :from => 26, :to => 43
 end
