189ca5046d59c520fe31f74681139475d4b9787a
ichy <ichylinux@gmail.com>
2013-08-24 12:40:58 +0900

    p4

--- careerlife/features/step_definitions/開発日記/2013-03-07.rb
+++ careerlife/features/step_definitions/開発日記/2013-03-07.rb
@@ -1,7 +1,7 @@
 # coding: UTF-8
 
 前提 /^rails g migration add_column_introduction_on_careers$/ do
-  show 'db/migrate/20130308164115_add_column_introduction_on_careers.rb', :as => 'edit'
+  show 'db/migrate/20130308164115_add_column_introduction_on_careers.rb', :as => ['auto', 'edit']
 end
 
 前提 /^rake db:migrate$/ do
--- careerlife/features/step_definitions/開発日記/2013-03-08.rb
+++ careerlife/features/step_definitions/開発日記/2013-03-08.rb
@@ -1,8 +1,8 @@
 # coding: UTF-8
 
 前提 /^日本語ロケールを用意$/ do
-  show 'config/locales/ja.yml'
-  show 'config/locales/messages.ja.yml'
+  show 'config/locales/ja.yml', :as => 'new'
+  show 'config/locales/messages.ja.yml', :as => 'new'
 end
 
 前提 /^erb を修正$/ do
@@ -10,5 +10,5 @@ end
 end
 
 前提 /^通知メッセージを修正$/ do
-  git_diff 'app/controllers/careers_controller.rb'
+  git_diff 'app/controllers/careers_controller.rb', :from => 42, :to => 82
 end
