0ab37016134879ecb23c020c2168650136cac0db
ichy <ichylinux@gmail.com>
2013-09-19 23:32:05 +0900

    puts

---
+++ lib/daddy/cucumber/puts.rb
@@ -0,0 +1,15 @@
+# coding: UTF-8
+
+module Daddy
+  module Cucumber
+    module Puts
+      
+      def pre(string)
+        puts "<pre>#{string}</pre>"
+      end
+
+    end
+  end
+end
+
+World(Daddy::Cucumber::Puts)
