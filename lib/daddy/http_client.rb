2c272f68bca83510a60b02490d833819de8172e8
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-10-28 12:50:49 +0900

    Basic認証

--- lib/daddy/http_client.rb
+++ lib/daddy/http_client.rb
@@ -8,19 +8,35 @@ module Daddy
     def initialize(url, options = {})
       @url = url
       @options = options
+      @cookie = nil
+      Rails.logger.debug "[HttpClient] url: #{@url}"
     end
     
     def get(path, params = {})
+      Rails.logger.debug "[HttpClient] path: #{path}"
+      Rails.logger.debug "[HttpClient] params: #{params}"
+
       response = connection.get(path, params) do |request|
         if @options[:auth_user] and @options[:auth_password]
           basic = Base64.encode64(@options[:auth_user] + ':' + @options[:auth_password])
           request.headers['Authorization'] = 'Basic ' + basic
         end
+        
+        if @cookie
+          request.headers['Cookie'] = @cookie
+        end
+        
+        params.each do |key, value|
+          request.params[key] = value
+        end
       end
+      
+      @cookie = response.headers['set-cookie']
       
       if block_given?
         yield response
       else
+        Rails.logger.debug "[HttpClient] stutus: #{response.status}"
         response.body.force_encoding('UTF-8')
       end
     end
