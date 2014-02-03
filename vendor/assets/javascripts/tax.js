da7a17e7fb2bf5b31d4b812178e5c0d457db8e36
ichy <ichylinux@gmail.com>
2014-02-03 23:31:30 +0900

    消費税の計算

--- vendor/assets/javascripts/tax.js
+++ vendor/assets/javascripts/tax.js
@@ -9,16 +9,38 @@ tax.TAX_TYPE_INCLUSIVE = 2;
 // 外税
 tax.TAX_TYPE_EXCLUSIVE = 3;
 
-tax.calcTaxAmount = function(taxType, amount) {
+tax.RATE_3 = Date.parse('1989-04-01');
+tax.RATE_5 = Date.parse('1997-04-01');
+tax.RATE_8 = Date.parse('2014-04-01');
+
+tax.getRateOn = function(date) {
+  if (typeof date === 'string') {
+    date = Date.parse(date);
+  }
+  
+  if (date >= tax.RATE_3 && date < tax.RATE_5) {
+    return 0.03;
+  } else if (date >= tax.RATE_5 && date < tax.RATE_8) {
+    return 0.05;
+  } else if (date >= tax.RATE_8) {
+    return 0.08;
+  }
+  
+  return 0;
+};
+
+tax.calcTaxAmount = function(taxType, date, amount) {
   if ( isNaN( amount ) ) {
     return '';
   }
 
+  var rate = this.getRateOn(date);
+
   if ( taxType == tax.TAX_TYPE_INCLUSIVE ) {
-    return parseInt(amount * 0.05 / 1.05);
+    return parseInt(amount * rate / (1 + rate));
   }
   else if ( taxType == tax.TAX_TYPE_EXCLUSIVE ) {
-    return parseInt(amount * 0.05); 
+    return parseInt(amount * rate); 
   }
 
   return '';
