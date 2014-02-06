1cf61624ea7fea84128e2d6e9f6bcaa64d02c646
ichy <ichylinux@gmail.com>
2014-02-07 00:13:44 +0900

    stringの拡張

--- vendor/assets/javascripts/string.js
+++ vendor/assets/javascripts/string.js
@@ -1,17 +1,23 @@
+if (typeof String.prototype.endsWith !== 'function') {
+  String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+  };
+};
+
 if (typeof String.prototype.isBlank !== 'function') {
-  String.prototype.isBlank = function(string) {
-    return string == null || string.length == '';
+  String.prototype.isBlank = function() {
+    return this.length == '';
   };
 };
 
 if (typeof String.prototype.isPresent !== 'function') {
-  String.prototype.isPresent = function(string) {
-    return string != null && string.length != '';
+  String.prototype.isPresent = function() {
+    return this.length != '';
   };
 };
 
-if (typeof String.prototype.endsWith !== 'function') {
-  String.prototype.endsWith = function(suffix) {
-    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+if (typeof String.prototype.toInt !== 'function') {
+  String.prototype.toInt = function() {
+    return parseInt(this) || 0;
   };
 };
