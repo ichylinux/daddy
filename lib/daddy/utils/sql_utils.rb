03bbbb612895443ad6d0afc6d8bdc0c4d78fc998
ichy <ichylinux@gmail.com>
2014-08-09 15:51:36 +0900

    リファクタリング

--- lib/daddy/utils/sql_utils.rb
+++ lib/daddy/utils/sql_utils.rb
@@ -1,5 +1,3 @@
-# coding: UTF-8
-
 module Daddy
   module Utils
     class SqlUtils
@@ -10,16 +8,16 @@ module Daddy
       
       def self.like(columns, keywords)
         target_columns = columns.is_a?(Array) ? columns : [columns]
-        
+
         sql = []
         sql[0] = '('
     
         keywords.gsub(/　/, ' ').split.each_with_index do |s, i|
           like = SqlUtils.escape_search(s)
-    
+
           sql[0]  << ' or ' if i > 0
           sql[0] << '('
-    
+
           target_columns.each_with_index do |column, j|
             sql[0] << ' or ' if j > 0
             sql[0] << "#{column} like ?"
