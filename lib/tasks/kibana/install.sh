9461ab8f0e390e10959065cfd0de514079c83926
ichy <ichylinux@gmail.com>
2014-11-21 14:37:27 +0900

    kibana

--- lib/tasks/kibana/install.sh
+++ lib/tasks/kibana/install.sh
@@ -6,4 +6,6 @@ VERSION=$1
 sudo rm -Rf /opt/kibana-${VERSION}
 sudo mv tmp/kibana-${VERSION} /opt/
 sudo ln -snf /opt/kibana-${VERSION} /opt/kibana
+
 cp -f tmp/config.js /opt/kibana/
+sudo cp -f tmp/nginx/kibana.conf /etc/nginx/conf.d/servers/
