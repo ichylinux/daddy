7e3e50e9d04c26e12d742d6663a3acf5f7a00bb4
ichy <ichylinux@gmail.com>
2016-01-22 09:08:04 +0900

    jenkins

--- cookbooks/jenkins/install.rb
+++ cookbooks/jenkins/install.rb
@@ -1,4 +1,4 @@
-directory 'tmp'
+ENV['DAD_JENKINS_URL'] ||= 'http://localhost:8080'
 
 package 'java-1.8.0-openjdk' do
   user 'root'
@@ -17,16 +17,51 @@ package 'jenkins' do
   user 'root'
 end
 
-directory '/var/lib/jenkins/plugins' do
-  user 'root'
-  group 'jenkins'
-  owner 'jenkins'
-end
-
 template '/etc/sysconfig/jenkins' do
   source File.join(File.dirname(__FILE__), 'jenkins.erb')
   user 'root'
   group 'root'
   owner 'root'
   mode '644'
+end
+
+service 'jenkins' do
+  user 'root'
+  action [:enable, :start]
+end
+
+directory '/var/lib/jenkins/plugins' do
+  user 'root'
+  group 'jenkins'
+  owner 'jenkins'
+end
+
+directory 'tmp'
+
+execute 'jenkins-cli.jar' do
+  cwd 'tmp'
+  command "wget #{ENV['DAD_JENKINS_URL']}/jnlpJars/jenkins-cli.jar"
+  not_if 'test -e jenkins-cli.jar'
+end
+
+@plugins = [
+  {:name => 'build-pipeline-plugin', :version => '1.4.3'},
+  {:name => 'git', :version => '2.2.5'},
+  {:name => 'git-client', :version => '1.10.2'},
+  {:name => 'rake', :version => '1.8.0'},
+  {:name => 'rubyMetrics', :version => '1.6.2'},
+  {:name => 'htmlpublisher', :version => '1.3'},
+  {:name => 'reverse-proxy-auth-plugin', :version => '1.4.0'},
+  {:name => 'thinBackup', :version => '1.7.4'}
+]
+@plugins.each do |plugin|
+  execute "/var/lib/jenkins/plugins/#{plugin[:name]}" do
+    cwd 'tmp'
+    command "java -jar jenkins-cli.jar -s #{ENV['DAD_JENKINS_URL']} install-plugin #{plugin[:name]}"
+  end
+end
+
+execute 'restart jenkins' do
+  cwd 'tmp'
+  command "java -jar jenkins-cli.jar -s #{ENV['DAD_JENKINS_URL']} safe-restart"
 end
--- lib/tasks/jenkins.rake
+++ lib/tasks/jenkins.rake
@@ -6,27 +6,6 @@ namespace :dad do
     desc 'Jenkinsをインストールします。'
     task :install do
       itamae 'jenkins/install.rb'
-
-      if false
-        plugins = [
-          {:name => 'build-pipeline-plugin', :version => '1.4.3'},
-          {:name => 'git', :version => '2.2.5'},
-          {:name => 'git-client', :version => '1.10.2'},
-          {:name => 'rake', :version => '1.8.0'},
-          {:name => 'rubyMetrics', :version => '1.6.2'},
-          {:name => 'htmlpublisher', :version => '1.3'},
-          {:name => 'reverse-proxy-auth-plugin', :version => '1.4.0'},
-          {:name => 'thinBackup', :version => '1.7.4'}
-        ]
-        plugins.each do |p|
-          download_path = "tmp/#{p[:name]}-#{p[:version]}.hpi"
-          unless File.exist?(download_path)
-            run "sudo wget http://updates.jenkins-ci.org/download/plugins/#{p[:name]}/#{p[:version]}/#{p[:name]}.hpi -O #{download_path}"
-          end
-          
-          run "sudo cp -f #{download_path} /var/lib/jenkins/plugins/#{p[:name]}.hpi"
-        end
-      end
     end
 
     namespace :nginx do
