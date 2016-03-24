97559692f6c6f920fc3968f15def827052e41c85
ichy <ichylinux@gmail.com>
2016-03-24 23:55:06 +0900

    capybara

--- lib/daddy/cucumber/helpers/assert.rb
+++ lib/daddy/cucumber/helpers/assert.rb
@@ -4,11 +4,11 @@ module Daddy
       def assert_url(path, params = {})
         # スクリーンショットの保存
         capture
-      
+
         # パスのチェック
         re = path.gsub(/\//, '\/')
-        assert /#{re}/ =~ current_path, "想定しているパスではありません。想定 #{path} : 実際 #{current_path}"
-        
+        assert has_current_path?(/#{re}/), "想定しているパスではありません。想定 #{path} : 実際 #{current_path}"
+
         # パラメータのチェック
         current_params = get_current_params()
         params.each do |key, value|
@@ -16,7 +16,7 @@ module Daddy
           assert /#{re}/ =~ current_params[key], "#{key}が想定している値ではありません。想定 #{value} : 実際 #{current_params[key]}"
         end
       end
-      
+
       def assert_visit(path, params = {})
         query_string = ''
         params.each do |key, value|
@@ -27,68 +27,68 @@ module Daddy
           end
           query_string += (key.to_s + '=' + value.to_s)
         end
-      
+
         visit path + query_string
         assert_url path, params
       end
-      
+
       def assert_fill_in(field_name, value)
         fill_in field_name, :with => value
         capture
       end
-      
+
       def assert_check(field_name)
         check field_name
         capture
       end
-      
+
       def assert_uncheck(field_name)
         uncheck field_name
         capture
       end
-      
+
       def assert_choose(field_name)
         choose field_name
         capture
       end
-      
+
       def assert_select(name, value)
         select value, :from => name
         capture
       end
-      
+
       def assert_selector(selector, options = {})
         assert page.has_selector?(selector), options[:message]
         capture if options[:capture]
       end
-      
+
       def assert_no_selector(selector, options = {})
         assert page.has_no_selector?(selector), options[:message]
         capture if options[:capture]
       end
-      
+
       def assert_present(value, message = nil)
         assert value.present?, message
       end
-      
+
       def assert_not_present(value, message = nil)
         assert ! value.present?, message
       end
-      
+
       def assert_content_type(content_type)
         assert_equal content_type, page.response_headers['Content-Type']
       end
-      
+
       def assert_iterate(array)
         iterated = false
-        
+
         array.each do |x|
           iterated ||= yield x
         end
-        
+
         fail 'テスト対象のデータがありません。' unless iterated
       end
-      
+
       def get_current_params
         ret = {}
         if current_url.include?('?')
