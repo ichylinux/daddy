bab452807ea2e3c08ec053c23bbab84f5e001750
ichy <ichylinux@gmail.com>
2017-05-22 00:17:51 +0900

    refactor

--- lib/daddy/cucumber.rb
+++ lib/daddy/cucumber.rb
@@ -1,14 +1,7 @@
 require 'capybara/cucumber'
-
-case ENV['DRIVER']
-when 'poltergeist'
-  require 'capybara/poltergeist'
-end
-
-Capybara.default_driver = (ENV['DRIVER'] || :selenium).to_sym
-Capybara.default_selector = :css
-
 require_relative 'cucumber/helpers'
+
+Capybara.default_selector = :css
 
 Before do
   resize_window(1280, 720)
--- lib/daddy/version.rb
+++ lib/daddy/version.rb
@@ -1,3 +1,3 @@
 module Daddy
-  VERSION = '0.5.24'
+  VERSION = '0.5.25'
 end
