f6f0727624da2359e1a8c13c722e01b9ac048fe5
ichy <ichylinux@gmail.com>
2017-05-21 14:23:12 +0900

    refactor

--- lib/daddy/cucumber.rb
+++ lib/daddy/cucumber.rb
@@ -8,22 +8,6 @@ end
 Capybara.default_driver = (ENV['DRIVER'] || :selenium).to_sym
 Capybara.default_selector = :css
 
-def override_method(obj, method_name, &block)
-  klass = class <<obj; self; end
-  klass.send(:undef_method, method_name)
-  klass.send(:define_method, method_name, block)
-end
-
-AfterConfiguration do |configuration|
-  feature_files =  configuration.feature_files
-
-  override_method(configuration, :feature_files) {
-    sorted_files = feature_files.sort do |x, y|
-      x <=> y
-    end
-  }
-end
-
 require_relative 'cucumber/helpers'
 
 Before do
