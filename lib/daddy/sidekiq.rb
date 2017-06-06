18c42ebed9852fbab5204c92b49a4e4c5f8d785c
ichy <ichylinux@gmail.com>
2017-06-07 00:17:21 +0900

    sidekiq

--- lib/daddy/sidekiq.rb
+++ lib/daddy/sidekiq.rb
@@ -3,6 +3,12 @@ redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379/0'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url, namespace: db_config['database'] }
+
+  begin
+    require 'sidekiq/scheduler'
+  rescue LoadError
+    puts 'sidekiq-scheduler is not loaded.'
+  end
 end
 
 Sidekiq.configure_client do |config|
--- lib/daddy/version.rb
+++ lib/daddy/version.rb
@@ -1,3 +1,3 @@
 module Daddy
-  VERSION = '0.5.27'
+  VERSION = '0.5.28'
 end
