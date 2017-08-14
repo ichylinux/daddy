71f981aab70ea321a156d87247dab171db1b30ae
ichy <ichylinux@gmail.com>
2017-08-14 15:47:48 +0900

    carrierwave

--- lib/daddy/uploader.rb
+++ lib/daddy/uploader.rb
@@ -1,6 +1,4 @@
-require 'carrierwave'
-
-CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:print:]]/
 
 CarrierWave.configure do |config|
   config.root = 
@@ -9,6 +7,23 @@ CarrierWave.configure do |config|
     else
       "/data/#{::Rails.application.class.parent_name.underscore}/#{::Rails.env}"
     end
+    
+  if File.exists?('config/aws.yml')
+    require 'carrierwave/storage/fog'
+    config.storage :fog
+    config.cache_storage :fog
+
+    config.fog_provider = 'fog/aws'
+    aws = YAML.load_file('config/aws.yml')
+
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: aws['s3']['access_key_id'],
+      aws_secret_access_key: aws['s3']['secret_access_key'],
+      region: aws['s3']['region']
+    }
+    config.fog_directory = aws['s3']['bucket']
+  end
 end
 
 module Daddy
