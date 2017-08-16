e048f2fbe43a37487be903de014ff9c6be8568bd
ichy <ichylinux@gmail.com>
2017-08-16 12:23:17 +0900

    docker

--- itamae/cookbooks/docker/install.rb
+++ itamae/cookbooks/docker/install.rb
@@ -11,17 +11,20 @@ when /rhel-6\.(.*?)/
   end
 
 when /rhel-7\.(.*?)/
-  template '/etc/yum.repos.d/docker.repo' do
-    user 'root'
-    owner 'root'
-    group 'root'
-    mode '644'
-  end
-  
-  package 'docker-engine' do
-    user 'root'
+  %w{ yum-utils device-mapper-persistent-data lvm2 }.each do |name|
+    package name do
+      user 'root'
+    end
   end
 
+  execute 'yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo' do
+    user 'root'
+    not_if 'test -e /etc/yum.repos.d/docker-ce.repo'
+  end
+  
+  package 'docker-ce' do
+    user 'root'
+  end
 else
   raise "unsupported operating system: #{os_version}"
 end
