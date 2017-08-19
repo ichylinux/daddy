7f54733f3a20085760e518ad5af9c3ebe3702aef
ichy <ichylinux@gmail.com>
2017-08-19 12:06:36 +0900

    letsencrypt

--- itamae/cookbooks/letsencrypt/install.rb
+++ itamae/cookbooks/letsencrypt/install.rb
@@ -1,18 +1,11 @@
 require 'daddy/itamae'
 
-%w{ git }.each do |name|
+%w{
+  epel-release
+  certbot
+  python2-certbot-nginx
+}.each do |name|
   package name do
     user 'root'
   end
-end
-
-directory '/opt/letsencrypt' do
-  user 'root'
-  owner ENV['USER']
-  group ENV['USER']
-  mode '755'
-end
-
-git '/opt/letsencrypt/certbot' do
-  repository 'https://github.com/certbot/certbot'
 end
