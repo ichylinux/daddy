7f2792cfe34dab475622f6e14c1f40e4171ce7c4
ichy <ichylinux@gmail.com>
2017-12-15 11:33:11 +0900

    capistrano

--- lib/capistrano/daddy.rb
+++ lib/capistrano/daddy.rb
@@ -1,4 +1,5 @@
 STDOUT.sync = true
 Dir.glob(File.join(File.dirname(__FILE__), 'tasks/*.rake')).each { |r| import r }
 
-set :passenger_restart_with_sudo, true
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
