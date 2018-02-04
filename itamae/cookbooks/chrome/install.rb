497a46826e463cb3545a5d2905654b5ab7ae0ef2
ichy <ichylinux@gmail.com>
2018-02-04 15:19:01 +0900

    chrome driver

--- itamae/cookbooks/chrome/install.rb
+++ itamae/cookbooks/chrome/install.rb
@@ -8,3 +8,12 @@ execute "download chromedriver-#{Daddy::CHROME_DRIVER_VERSION}" do
   EOF
   not_if "sha256sum -c #{::File.join(::File.dirname(__FILE__), "chromedriver_linux64-#{Daddy::CHROME_DRIVER_VERSION}_sha256sum.txt")}"
 end
+
+execute "install chromedriver-#{Daddy::CHROME_DRIVER_VERSION}" do
+  cwd 'tmp'
+  command <<-EOF
+    unzip chromedriver_linux64-#{Daddy::CHROME_DRIVER_VERSION}.zip
+    sudo mv chromedriver /usr/local/bin/
+  EOF
+  not_if "/usr/local/bin/chromedriver -v | grep 'ChromeDriver 2.35'"
+end
