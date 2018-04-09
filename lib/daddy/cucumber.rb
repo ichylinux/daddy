ee7b712e095675408a1f9737deafe12bc5d70cd9
ichy <ichylinux@gmail.com>
2018-04-10 01:31:56 +0900

    capybara 3.0

--- lib/daddy/cucumber.rb
+++ lib/daddy/cucumber.rb
@@ -1,8 +1,4 @@
-require 'capybara/cucumber'
 require_relative 'cucumber/helpers'
-
-Capybara.default_selector = :css
-Capybara.server = :puma if defined?(Puma)
 
 Before do
   resize_window(1280, 720)
