c0e3a13c6ccda09fb19af485112cd9b67a5439e3
ichy <ichylinux@gmail.com>
2018-04-14 10:02:25 +0900

    chromedriver

--- itamae/cookbooks/selenium/chromedriver.rb
+++ itamae/cookbooks/selenium/chromedriver.rb
@@ -1,19 +1,23 @@
+require 'daddy'
+
 directory 'tmp'
 
-execute "download chromedriver-#{Daddy::CHROME_DRIVER_VERSION}" do
+version = ENV['CHROME_DRIVER_VERSION'] || Daddy::CHROME_DRIVER_VERSION
+
+execute "download chromedriver-#{version}" do
   cwd 'tmp'
   command <<-EOF
-    curl -o chromedriver_linux64-#{Daddy::CHROME_DRIVER_VERSION}.zip \
-        https://chromedriver.storage.googleapis.com/#{Daddy::CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
+    curl -o chromedriver_linux64-#{version}.zip \
+        https://chromedriver.storage.googleapis.com/#{version}/chromedriver_linux64.zip
   EOF
-  not_if "sha256sum -c #{::File.join(::File.dirname(__FILE__), "chromedriver_linux64-#{Daddy::CHROME_DRIVER_VERSION}_sha256sum.txt")}"
+  not_if "sha256sum -c #{::File.join(::File.dirname(__FILE__), "chromedriver_linux64-#{version}_sha256sum.txt")}"
 end
 
-execute "install chromedriver-#{Daddy::CHROME_DRIVER_VERSION}" do
+execute "install chromedriver-#{version}" do
   cwd 'tmp'
   command <<-EOF
-    unzip chromedriver_linux64-#{Daddy::CHROME_DRIVER_VERSION}.zip
+    unzip chromedriver_linux64-#{version}.zip
     sudo mv -f chromedriver /usr/local/bin/
   EOF
-  not_if "/usr/local/bin/chromedriver -v | grep 'ChromeDriver 2.35'"
+  not_if "/usr/local/bin/chromedriver -v | grep 'ChromeDriver #{version}'"
 end
