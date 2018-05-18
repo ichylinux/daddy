786ac6d60bf00ed68d8ea107b8680afac552ca0c
ichy <ichylinux@gmail.com>
2018-05-18 16:30:27 +0900

    tensorflow

--- itamae/cookbooks/tensorflow/install.rb
+++ itamae/cookbooks/tensorflow/install.rb
@@ -37,7 +37,7 @@ local_ruby_block 'install tensorflow' do
 PYTHON_BIN_PATH=/usr/local/bin/python3 ./configure
 bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package 
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-sudo pip3 install /tmp/tensorflow_pkg/tensorflow-1.7.1-cp36-cp36m-linux_x86_64.whl
+sudo pip3 install /tmp/tensorflow_pkg/tensorflow-1.8.0-cp36-cp36m-linux_x86_64.whl
     EOF
   end
 end
