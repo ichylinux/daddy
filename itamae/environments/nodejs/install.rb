29cbbc2a0648c77245e15d1e4e51a920ff522fcd
ichy <ichylinux@gmail.com>
2018-06-02 19:41:49 +0900

    nodejs

--- itamae/environments/nodejs/install.rb
+++ itamae/environments/nodejs/install.rb
@@ -2,6 +2,10 @@ template '/etc/yum.repos.d/nodesource.repo' do
   user 'root'
 end
 
+template '/etc/pki/rpm-gpg/NODESOURCE-GPG-SIGNING-KEY-EL' do
+  user 'root'
+end
+
 package 'nodejs' do
   user 'root'
 end
