dbd7e232c9c4be226c488ab173597fac47362ef3
ichy <ichylinux@gmail.com>
2018-08-05 13:18:41 +0900

    bundle update

--- lib/daddy/itamae/config.rb
+++ lib/daddy/itamae/config.rb
@@ -4,7 +4,7 @@ require 'daddy/utils/config'
 module Daddy
 
   def self.config
-    @_config ||= Daddy::Utils::Config.new(YAML.load_file('config', 'daddy.yml'))
+    @_config ||= Daddy::Utils::Config.new(YAML.load_file('config/daddy.yml'))
   end
 
 end
