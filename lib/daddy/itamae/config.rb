eaebedecc31755cd0ceca35c01dd7c0a867885e3
ichy <ichylinux@gmail.com>
2018-12-08 15:51:33 +0900

    bugfix

--- lib/daddy/itamae/config.rb
+++ lib/daddy/itamae/config.rb
@@ -6,9 +6,9 @@ module Daddy
   def self.config
     if @_config.nil?
       daddy_yml = File.join('config', 'daddy.yml')
-      puts daddy_yml
       if File.exist?(daddy_yml)
-        @_config = Daddy::Utils::Config.new(daddy_yml)
+        parsed = ERB.new(File.read(daddy_yml), 0, '-').result
+        @_config = Daddy::Utils::Config.new(YAML.load(parsed))
       else
         @_config = Daddy::Utils::Config.new
       end
