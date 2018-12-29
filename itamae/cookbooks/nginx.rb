e74a60199148eb8c1f96d760a0c22e974df3df83
ichy <ichylinux@gmail.com>
2018-12-29 20:04:56 +0900

    nginx

--- itamae/cookbooks/nginx.rb
+++ itamae/cookbooks/nginx.rb
@@ -1,5 +1,5 @@
 require 'daddy/itamae'
 
-include_recipe 'daddy::nginx::start'
-include_recipe 'daddy::nginx::install'
 include_recipe 'daddy::nginx::stop'
+include_recipe 'daddy::nginx::install'
+include_recipe 'daddy::nginx::start'
