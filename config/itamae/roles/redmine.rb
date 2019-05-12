649fac21348488e7a7f88142dd5c446eef3139c0
ichy <ichylinux@gmail.com>
2019-05-12 21:05:49 +0900

    redmine

--- config/itamae/roles/redmine.rb
+++ config/itamae/roles/redmine.rb
@@ -1,2 +1,6 @@
+ENV['APPLICATION_NAME'] ||= 'redmine'
+ENV['APPLICATION_ENV'] ||= 'production'
+ENV['APPLICATION_ROOT'] ||= '/opt/redmine/current'
+
 include_recipe 'daddy::nginx'
 include_recipe 'daddy::redmine'
