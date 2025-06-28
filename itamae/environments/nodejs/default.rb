9ef56c35c150090a1580d2410548eb68a9f1c14a
ichy <ichylinux@gmail.com>
2025-06-28 12:16:23 +0900

    AlmaLinux8 環境で mitamae でリソース file の root 権限での実行が正常に機能しないためレシピを修正

--- itamae/environments/nodejs/default.rb
+++ itamae/environments/nodejs/default.rb
@@ -5,10 +5,20 @@ file '/etc/yum.repos.d/nodesource-nodejs.repo' do
   mode '644'
 end
 
+execute 'place /etc/yum.repos.d/nodesource-nodejs.repo' do
+  user 'root'
+  command <<-EOF
+    set -eu
+    sudo cp -f #{::File.join(::File.dirname(__FILE__), 'files/etc/yum.repos.d/nodesource-nodejs.repo')} /etc/yum.repos.d/nodesource-nodejs.repo
+    sudo chmod 644 /etc/yum.repos.d/nodesource-nodejs.repo
+  EOF
+  not_if "diff #{::File.join(::File.dirname(__FILE__), 'files/etc/yum.repos.d/nodesource-nodejs.repo')} /etc/yum.repos.d/nodesource-nodejs.repo"
+end
+
 execute 'yum clean all --enablerepo=nodesource-nodejs' do
   user 'root'
   action :nothing
-  subscribes :run, "file[/etc/yum.repos.d/nodesource-nodejs.repo]", :immediately
+  subscribes :run, "execute[place /etc/yum.repos.d/nodesource-nodejs.repo]", :immediately
 end
 
 package 'nodejs' do
@@ -17,11 +27,20 @@ package 'nodejs' do
   options '--enablerepo=nodesource-nodejs'
 end
 
-file '/etc/yum.repos.d/yarn.repo' do
+execute 'place /etc/yum.repos.d/yarn.repo' do
   user 'root'
-  owner 'root'
-  group 'root'
-  mode '644'
+  command <<-EOF
+    set -eu
+    sudo cp -f #{::File.join(::File.dirname(__FILE__), 'files/etc/yum.repos.d/yarn.repo')} /etc/yum.repos.d/yarn.repo
+    sudo chmod 644 /etc/yum.repos.d/yarn.repo
+  EOF
+  not_if "diff #{::File.join(::File.dirname(__FILE__), 'files/etc/yum.repos.d/yarn.repo')} /etc/yum.repos.d/yarn.repo"
+end
+
+execute 'yum clean all --enablerepo=yarn' do
+  user 'root'
+  action :nothing
+  subscribes :run, "execute[place /etc/yum.repos.d/yarn.repo]", :immediately
 end
 
 package 'yarn' do
